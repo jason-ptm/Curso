b3473786f7bd4b9acce20fdbb0b51a4c5088934b
juan <elmapa2003@gmail.com>
2021-09-22 04:34:24 -0500

    se eliminaron las carpetas componentes

--- Aprendiendo/src/app/app.module.ts
+++ Aprendiendo/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MiComponente } from './components/mi-componente/mi-componente.component';
-import { ArticuloComponent } from './components/articulo/articulo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MiComponente,
-    ArticuloComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
